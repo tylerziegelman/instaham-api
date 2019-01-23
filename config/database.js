50ae3abd7f51d6bdd4313da8bc3cf85928487215
tylerziegelman <tylerziegelman+git@gmail.com>
2019-01-22 18:36:29 -0700

    finish deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
 'use strict'
-const url = require('url-parse')
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+
+const url = require('url-parse')
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 module.exports = {
   /*
@@ -49,17 +51,25 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
-  },
+  // mysql: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     port: Env.get('DB_PORT', ''),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   },
+  //   debug: Env.get('DB_DEBUG', false)
+  // },
+
+  connection: {
+    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+    port: Env.get('DB_PORT', ''),
+    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+}, 
 
   /*
   |--------------------------------------------------------------------------
